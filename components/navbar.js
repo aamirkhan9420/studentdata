a456b9d3cb7a1ae28c4d1082064db6bd8174ab56
aamirkhan9420 <aamirkhan.z9373@gmail.com>
2022-06-26 23:11:13 +0530

    doneall

---
+++ components/navbar.js
@@ -0,0 +1,8 @@
+function navbar() {
+  return ` <div class="divs" id="web15">web15 - 0</div>
+    <div class="divs" id="web16">web16 - 0</div>
+    <div class="divs" id="web17">web17 - 0</div>
+    <div class="divs" id="web18">web18 - 0</div>
+    <div class="divs" id="web19">web19 - 0</div>`;
+}
+export default navbar;
